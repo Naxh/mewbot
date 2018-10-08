754bffcbee90dcff424d5638453cf45c6c5c02b7
Oyindoubra Akposeye <36546002+Dyleee@users.noreply.github.com>
2018-10-08 17:30:15 +0100

    Update mewbot.py

--- mewbot.py
+++ mewbot.py
@@ -421,7 +421,7 @@ async def info(ctx):
         except TypeError as e:
             await ctx.send(f'You need to `;select` a pokemon or you haven\'t started <@{ctx.author.id}>')
 	pns = str(pn)
-	r = requests.get('http://pokeapi.co/api/v2/pokemon/' + pns.lower() + '/')
+        r = requests.get('http://pokeapi.co/api/v2/pokemon/' + pns.lower() + '/')
 	rJson = r.json()
 	types = [t['type']['name'] for t in rJson['types']]
 	tlist = ", ".join(types)
