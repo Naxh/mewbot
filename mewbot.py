bd3da9ef58acd862d1205d1bab4e31c9e9bcf3df
Oyindoubra Akposeye <36546002+Dyleee@users.noreply.github.com>
2018-10-08 11:41:28 +0100

    Update mewbot.py

--- mewbot.py
+++ mewbot.py
@@ -293,7 +293,7 @@ async def pokemon(ctx):
     precord = [record['pnum'] for record in pk1]	   
     embed = discord.Embed(title='Your Pokemon List')	    
     for pn in precord:
-	nr = (nrecord[pn])
+	nr = nrecord[pn]
 	embed.add_field(name=f'󠂪󠂪󠂪󠂪{nr}󠂪󠂪', value=f"Number-{pn}", inline=False)
     embed.set_footer(text='Upvote the Bot!')
     await ctx.send(embed=embed)
