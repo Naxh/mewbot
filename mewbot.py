ad7d087f8890de28102ed132ec129cd9d5f13103
Oyindoubra Akposeye <36546002+Dyleee@users.noreply.github.com>
2018-10-09 12:42:47 +0100

    Update mewbot.py

--- mewbot.py
+++ mewbot.py
@@ -242,7 +242,6 @@ react_to_starter = {
 }
 @bot.command(name="start")
 async def start_journey(ctx):
-	bot.logger
 	embed = discord.Embed(title="Select a Starter!", description="Choose any of the Starters!")
 	embed.add_field(name="...", value="You've been hypnotized by Mew, and instead of the normal starters, you are forced to pick between \n-Flowin, the Grass type fakemon, \n-Flire the fire type fakemon and \n-Aquino, the water type.")
 	embed.set_thumbnail(url="https://nerdist.com/wp-content/uploads/2016/02/Screen-Shot-2016-02-02-at-12.05.40-PM-615x346.png")
@@ -301,7 +300,6 @@ async def start_journey(ctx):
 
 @bot.command()
 async def pokemon(ctx):
-	bot.logger
 	pconn = await asyncpg.connect(dburl)
 	nquery = "SELECT pokname, pnum FROM pokes WHERE ownerid = {}".format(ctx.author.id)
 	pk1 = await pconn.fetch(nquery)
