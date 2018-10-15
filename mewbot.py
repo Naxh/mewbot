20fd74981f450cbf93fed9f117915b1c57d1ed3a
Oyindoubra Akposeye <36546002+Dyleee@users.noreply.github.com>
2018-10-15 13:03:27 +0100

    Update mewbot.py

--- mewbot.py
+++ mewbot.py
@@ -473,7 +473,7 @@ async def pokemon(ctx, val=None):
 	nrecord = [record['pokname'] for record in pk1]
 	precord = [record['pnum'] for record in pk1]
 	embed = discord.Embed(title='Your Pokemon List', color=0xffb6c1)
-	for pn in precord[enum:snum]:
+	for pn in precord[0:11]:
 		await ctx.send(pn)
 		try:
 			nr = nrecord[pn-1]
