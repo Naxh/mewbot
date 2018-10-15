552a7e44a3385071a0cb918313c48680a37f2854
Oyindoubra Akposeye <36546002+Dyleee@users.noreply.github.com>
2018-10-15 06:47:47 +0100

    Update mewbot.py

--- mewbot.py
+++ mewbot.py
@@ -1411,49 +1411,7 @@ async def on_message(message):
 
 		
 		
-		
-		
-		
-		
-		
-		
 
-
-class DiscordBotsOrgAPI:
-    """Handles interactions with the discordbots.org API"""
-
-    def __init__(self, bot):
-        self.bot = bot
-        self.token = dbltoken  #  set this to your DBL token
-        self.dblpy = dbl.Client(self.bot, self.token)
-        self.bot.loop.create_task(self.update_stats())
-
-    async def update_stats(self):
-        """This function runs every 30 minutes to automatically update your server count"""
-
-        while True:
-            logger.info('attempting to post server count')
-            try:
-                await self.dblpy.post_server_count()
-                logger.info('posted server count ({})'.format(len(self.bot.guilds)))
-            except Exception as e:
-                logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
-            await asyncio.sleep(1800)
-
-
-def setup(bot):
-    global logger
-    logger = logging.getLogger('bot')
-    bot.add_cog(DiscordBotsOrgAPI(bot))
-		
-
-	
-if __name__ == "__main__":
-	try:
-		bot.load_extension(DiscordBotsOrgApi)
-	except Exception as e:
-		exc = '{}: {}'.format(type(e).__name__, e)
-		print('Failed to load extension {}\n{}'.format(extension, exc))
 bot.run(TOKEN)
 
 			
