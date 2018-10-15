f32e35741f2d91f93f304225b7eed14dacacac65
Oyindoubra Akposeye <36546002+Dyleee@users.noreply.github.com>
2018-10-15 05:41:01 +0100

    Update mewbot.py

--- mewbot.py
+++ mewbot.py
@@ -427,7 +427,7 @@ async def pokemon(ctx, val=None):
 	else:
 		val = int(val)
 		enum = (rnum/int(val))
-		snum = ((10*val)-10)
+		snum = ((10*val)-8)
 	pconn = await bot.db.acquire()
 	nquery = f"SELECT pokname, pnum FROM pokes WHERE ownerid = {ctx.author.id} AND pnum BETWEEN {enum} AND {rnum} ORDER BY pnum"
 	pk1 = await pconn.fetch(nquery)
