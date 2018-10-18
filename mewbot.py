43c309231985ff10735027b1146e5fc154d0fa16
Oyindoubra Akposeye <36546002+Dyleee@users.noreply.github.com>
2018-10-18 20:03:17 +0100

    Update mewbot.py

--- mewbot.py
+++ mewbot.py
@@ -1792,7 +1792,7 @@ async def trade(ctx, user: discord.Member, creds: int, poke: int):
         e.set_footer(text="Say Yes to accept")
         await ctx.send(embed=e)
         def check(m):
-            m.author == user.id
+            m.author == user.id and m.content in ('Yes', 'yes', 'No', 'no')
         try:
             msg = await bot.wait_for('message', check=check, timeout=30)
         except asyncio.TimeoutError:
